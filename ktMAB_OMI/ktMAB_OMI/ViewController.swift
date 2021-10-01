30c94498f3682a33a81a52af9fe0e0f55b623673
kenny2028 <78673397+kenny2028@users.noreply.github.com>
2021-10-01 17:47:07 -0600

    Add files via upload

---
+++ ktMAB_OMI/ktMAB_OMI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ktMAB_OMI
+//
+//  Created by Kenny Trang on 10/1/21.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
